b1e2cfa629d3d10f5bbdc410b497d4abafbe9b33
Marco Rea <marco.reaoviedo@servexternos.gruposantander.com>
2022-10-04 12:45:09 -0500

    Update file

--- ansible/library/sgt_onefcc_servicenow_release.py
+++ ansible/library/sgt_onefcc_servicenow_release.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
-from lib2to3.pgen2 import token
+#from lib2to3.pgen2 import token
 import sys
 #from ansible.module_utils.basic import AnsibleModule
-from requests.adapters import HTTPAdapter
+#from requests.adapters import HTTPAdapter
 import requests
 import json
 #import os
@@ -14,25 +14,25 @@ import ssl
 #import chardet
 #import mimetypes
 #import sys
-import logging
-import http.client as http_client
+#import logging
+#import http.client as http_client
 #from logging import getLogger, INFO
 #from concurrent_log_handler import ConcurrentRotatingFileHandler
 from datetime import datetime
-import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-http_client.HTTPConnection.debuglevel = 1
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
-requests_log = logging.getLogger("requests.packages.urllib3")
-requests_log.setLevel(logging.DEBUG)
-requests_log.propagate = True
+#import urllib3
+#urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+#http_client.HTTPConnection.debuglevel = 1
+#logging.basicConfig()
+#logging.getLogger().setLevel(logging.DEBUG)
+#requests_log = logging.getLogger("requests.packages.urllib3")
+#requests_log.setLevel(logging.DEBUG)
+#requests_log.propagate = True
 
 
 
 __metaclass__ = type
 
-# https://santandertest.service-now.com
+SGT_ONEFCC_BASE_RELEASE = "https://santandertest.service-now.com"
 #SGT_ONEFCC_URI_RELEASE = '/api/now/v2/table/rm_release'
 SGT_ONEFCC_URI_RELEASE = '/public/v2/users'
 SGT_ONEFCC_LOG = True
@@ -56,11 +56,43 @@ def log(message):
     if SGT_ONEFCC_LOG:
         print(str(datetime.now()) + ": " + message)
 
+def create_releasev2(**release):
+    #servicenow_adapter = HTTPAdapter(max_retries=retries)
+    #session = requests.Session()
+
+    response = None
+    
+    release["sn_base"] = SGT_ONEFCC_BASE_RELEASE if "sn_base" not in release else release["sn_base"]
+    release["sn_uri"] = SGT_ONEFCC_URI_RELEASE if "sn_user" not in release else release["sn_uri"]
+    release["sn_user"] = "" if "sn_user" not in release else release["sn_user"]
+    release["sn_pass"] = "" if "sn_pass" not in release else release["sn_pass"]
+    release["data"] = None if "data" not in release else release["data"]
+    release["headers"] = None if "headers" not in release else release["headers"]
+    release["params"] = None if "params" not in release else release["params"]
+    release["authType"] = "Basic" if "authType" not in release else release["authType"]
+    release["timeout"] = 300 if "timeout" not in release else release["timeout"]
+
+    endpoint = release["sn_base"] + release["sn_uri"]
+
+    print(release)
+
+    try:
+        if release["authType"].lower() == "basic":
+            response = requests.post(url=endpoint, headers=release["headers"], auth=(
+                release["sn_user"], release["sn_pass"]), data=release["data"], params=release["params"], timeout=release["timeout"])
+        else:
+            response = requests.post(url=endpoint, headers=release["headers"], data=release["data"], params=release["params"], timeout=release["timeout"])
+
+    except Exception as e:
+        response = {"mensaje": "ERROR, no se pudo crear la release: " + str(e) }
+        log("ERROR, no se pudo crear la release: " + str(e))
+
+    return response
 
 def create_release(sn_user, sn_pass, sn_instance, data, uri="/api/now/v2/table/rm_release", retries=3, authType="basic", params=None, headers=None, timeout=300):
 
-    servicenow_adapter = HTTPAdapter(max_retries=retries)
-    session = requests.Session()
+    #servicenow_adapter = HTTPAdapter(max_retries=retries)
+    #session = requests.Session()
 
     response = None
     endpoint = sn_instance + uri
@@ -72,8 +104,8 @@ def create_release(sn_user, sn_pass, sn_instance, data, uri="/api/now/v2/table/r
             response = requests.post(endpoint, headers=headers, auth=(
                 sn_user, sn_pass), data=str(data), params=params, timeout=timeout)
         else:
-            response = requests.post(
-                url=endpoint, headers=headers, data=str(data))
+            print(str(data))
+            response = requests.post(url=endpoint, headers=headers, data=data)
 
     except Exception as e:
         log("ERROR, no se pudo crear la release: " + str(e))
@@ -173,27 +205,52 @@ def main():
 
 
 def local():
-    token = "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"
+    token = "d48cf0a45d36311ab405e1fd4cd1b533da3b2f22b5dceb30c48b8f6151eb3bec"
     data = {
         "name": "Foo Jhonson",
         "gender": "male",
-        "email": "foo.jhonson@tes.local",
+        "email": "foo.jhonson@local.email",
         "status": "active"
         }
 
-    headers = {
-        "Authorization": "Bearer "+token
-    }
+    headers = {'Authorization': "Bearer {}".format(token)}
+
     
     #query = { 'sysparm_fields': 'sys_id,number' }
     query = None
 
     return create_release("", "", "https://gorest.co.in", data, "/public/v2/users", 3, "token", query, headers, 300)
 
+def localv2():
+    token = "d48cf0a45d36311ab405e1fd4cd1b533da3b2f22b5dceb30c48b8f6151eb3bec"
+    data = {
+        "name": "Foo Jhonson",
+        "gender": "male",
+        "email": "foo.jhonson@localv2.email",
+        "status": "active"
+        }
+
+    headers = {'Authorization': "Bearer {}".format(token)}
+
+    release_params = {
+        "sn_base": "https://gorest.co.in",
+        "sn_uri": "/public/v2/users",
+        "sn_user": "",
+        "sn_pass": "",
+        "data": data,
+        "headers": headers,
+        "authType": "TOKEN"
+    }
+    
+    #query = { 'sysparm_fields': 'sys_id,number' }
+    
+
+    return create_releasev2(**release_params)
+
 if __name__ == '__main__':
     if sys.argv[1] == "local":
         print("En modo local")
-        respuesta = local()
+        respuesta = localv2()
         print(respuesta.json())
         # local()
     else:
